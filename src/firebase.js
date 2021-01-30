b26dd1340fb78e19568e30909d97ed05d695cf21
Kabilesh T <kabilesh.t@sap.com>
2021-01-30 17:00:32 +0530

    Initial cleanup and firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBHwwdcd_HLAEAg2E5CaSZU5wxPZ6ZQOS0",
+  authDomain: "whatsapp-clone-52333.firebaseapp.com",
+  projectId: "whatsapp-clone-52333",
+  storageBucket: "whatsapp-clone-52333.appspot.com",
+  messagingSenderId: "239853972163",
+  appId: "1:239853972163:web:3a41b0bfa95ed9fab41076"
+};
